5fad4d4f99753f621c246f444129581688a38a56
ZakYeo <zakyeomanson@gmail.com>
2023-01-12 22:47:11 +0000

    removed constants

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,17 +1,2 @@
 export default {
-    metres_to_miles: 1609.34,
-    nt_url: "https://www.nationaltrust.org.uk/",
-    nt_phone: "+441202707744",
-    nt_mail: "nationaltrust@nationaltrust.org.uk",
-    nav_map: "Add",
-    nav_list: "View",
-    events_on: "Events On", 
-    filter_opentoday: "Open today",
-    filter_closedtoday: "Closed today",
-    filter_partialtoday: "Partially open today",
-    search_placeholder: "Enter Your Search Here",
-    search: "Search",
-    cancel: "Cancel",
-    list_title: "View Receipts",
-    map_title: "Add Receipts"
 }
