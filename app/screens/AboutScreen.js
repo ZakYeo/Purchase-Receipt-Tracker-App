1ae6a0dc48158d44ffcbacfcbd746eca708f2c74
ZakYeo <zakyeomanson@gmail.com>
2023-01-13 00:56:26 +0000

    Updated about screen

--- app/screens/AboutScreen.js
+++ app/screens/AboutScreen.js
@@ -17,7 +17,7 @@ function AboutScreen() {
             size={200}
         />
         <Text style={{fontSize: 30, textDecorationLine: 'underline', paddingBottom: 20}}>Receipt Ranger</Text>
-        <Text>Receipt Ranger is a mobile app written in React Native for the sole purpose of ...</Text>
+        <Text>Receipt Ranger is a mobile app written in React Native. Track any kind of expense or receipt easily via manual, camera, or library inputs. With this app, keeping track of your expenses and receipts has never been easier, and thanks to the optical character recognition features it's very easy to track receipts on-the-go!</Text>
       </View>
     </ScrollView>
     );
